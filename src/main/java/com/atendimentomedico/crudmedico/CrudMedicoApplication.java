37dd601afbcd6c8668055274ac15e1d2adbbbfc5
duartemarco <marcoduarte96sm@gmail.com>
2023-12-14 09:11:21 -0300

    feat spring security com environment variable

--- src/main/java/com/atendimentomedico/crudmedico/CrudMedicoApplication.java
+++ src/main/java/com/atendimentomedico/crudmedico/CrudMedicoApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication
 public class CrudMedicoApplication {
